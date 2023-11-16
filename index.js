5a04a00e75cca7681d7022be3e661f40868828f9
PamelitaDandolo <pamela.senhoradosventos@gmail.com>
2023-11-15 22:10:41 -0300

    identação de cli e index js

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
-// fazer a importação do file system que é a biblioteca nativa do node.js, não é necessário fazer instalação
-const fs = require("fs")
+// importação do file system que é a biblioteca nativa do node.js, não é necessário fazer instalação
+const fs = require('fs');
 
 function soma(a, b) {
   return a + b;
 }
 
-function lerArquivo(caminhoDoArquivo){
-  return new Promise(function (resolve, reject){
+function lerArquivo(caminhoDoArquivo) {
+  return new Promise(function (resolve, reject) {
     fs.readFile(caminhoDoArquivo, "utf8", (err, data) => {
-      if(err) reject(err); // se tiver erro, lançar o erro
+      if (err) reject(err); // se tiver erro, lançar o erro
 
-      resolve(data); //parâmetro data é onde está o conteúdo do arquivo
+      resolve(data); // parâmetro data é onde está o conteúdo do arquivo
     });
-
-  })
+  });
 }
 
 module.exports = { soma, lerArquivo };
